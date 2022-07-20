2174505630c0625002179c4ffcd2c521d5ba082a
Zhouyou Gu <zhouyou.gu@sydney.edu.au>
2022-07-20 22:53:46 +1000

    move populatearp

--- scratch/wifi-test/scratch-wifi-single-ap-rate-test.cc
+++ scratch/wifi-test/scratch-wifi-single-ap-rate-test.cc
@@ -10,12 +10,12 @@
 #include "ns3/udp-server.h"
 #include "ns3/boolean.h"
 #include "ns3/ipv4-global-routing-helper.h"
-#include "populatearp/populatearpcache.h"
 
 
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("wifi-single-ap-rate-test");
+void PopulateARPcache();
 
 int main (int argc, char *argv[])
 {
@@ -154,4 +154,41 @@ int main (int argc, char *argv[])
   Simulator::Destroy ();
 
   return 0;
+}
+
+void PopulateARPcache () {
+  Ptr<ArpCache> arp = CreateObject<ArpCache>();
+  arp->SetAliveTimeout(Seconds(3600 * 24 * 365));
+
+  for (NodeList::Iterator i = NodeList::Begin(); i != NodeList::End(); ++i) {
+    Ptr<Ipv4L3Protocol> ip = (*i)->GetObject<Ipv4L3Protocol>();
+    NS_ASSERT (ip != 0);
+    ObjectVectorValue interfaces;
+    ip->GetAttribute("InterfaceList", interfaces);
+
+    for (ObjectVectorValue::Iterator j = interfaces.Begin(); j != interfaces.End(); j++) {
+      Ptr<Ipv4Interface> ipIface = (*j).second->GetObject<Ipv4Interface>();
+      NS_ASSERT (ipIface != 0);
+      Ptr<NetDevice> device = ipIface->GetDevice();
+      NS_ASSERT (device != 0);
+      Mac48Address addr = Mac48Address::ConvertFrom(device->GetAddress());
+
+      for (uint32_t k = 0; k < ipIface->GetNAddresses(); k++) {
+        Ipv4Address ipAddr = ipIface->GetAddress(k).GetLocal();
+        if (ipAddr == Ipv4Address::GetLoopback())
+          continue;
+        NS_LOG_FUNCTION("PopulateARPcacheForWiFiInterface" << "Node id: " << device->GetNode()->GetId());
+        NS_LOG_FUNCTION("PopulateARPcacheForWiFiInterface" << "device name id: " << device->GetInstanceTypeId());
+        NS_LOG_FUNCTION("PopulateARPcacheForWiFiInterface" << "if id: " << ipIface->GetMetric() << "/" << j->first);
+        NS_LOG_FUNCTION("PopulateARPcacheForWiFiInterface" << "address id: " << ipAddr);
+
+        ArpCache::Entry *entry = arp->Add(ipAddr);
+        Ipv4Header ipv4Hdr;
+        ipv4Hdr.SetDestination(ipAddr);
+        Ptr<Packet> p = Create<Packet>(100);
+        entry->MarkWaitReply(ArpCache::Ipv4PayloadHeaderPair(p, ipv4Hdr));
+        entry->MarkAlive(addr);
+      }
+    }
+  }
 }
